4380b79c2d33fd2ae77e5973e2728475ba0a1c2f
Sweeper777 <sumulang@gmail.com>
2018-06-03 18:39:47 +0800

    added markers on map

--- PortsAndSurveyors/Form1.cs
+++ PortsAndSurveyors/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
 
 namespace PortsAndSurveyors {
     public partial class Form1 : Form {
@@ -20,8 +23,17 @@ namespace PortsAndSurveyors {
             gmap.MinZoom = 2;
             gmap.MaxZoom = 14;
             gmap.Position = new PointLatLng(0, 0);
-            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gmap.MapProvider = GoogleMapProvider.Instance;
             GMaps.Instance.Mode = AccessMode.ServerOnly;
+
+            gmap.ShowCenter = false;
+
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(48.8617774, 2.349272),
+                GMarkerGoogleType.red);
+            markers.Markers.Add(marker);
+            gmap.Overlays.Add(markers);
         }
     }
 }
