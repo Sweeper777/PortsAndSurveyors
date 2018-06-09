8b03e5811b182ae89fb975326b62e82af9439421
Sweeper777 <sumulang@gmail.com>
2018-06-09 09:24:33 +0100

    added model class

---
+++ PortsAndSurveyors/PortsAndSurveyorsData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using GMap.NET;
+
+namespace PortsAndSurveyors {
+
+    //    var portsAndASurveyorsData = PortsAndASurveyorsData.FromJson(jsonString);
+
+    public partial class PortsAndASurveyorsData {
+        [JsonProperty("ports")]
+        public List<Port> Ports {
+            get; set;
+        }
+
+        [JsonProperty("surveyors")]
+        public List<Surveyor> Surveyors {
+            get; set;
+        }
+    }
+
+    public partial class Port {
+        [JsonProperty("name")]
+        public string Name {
+            get; set;
+        }
+
+        [JsonProperty("latitude")]
+        public double Latitude {
+            get; set;
+        }
+
+        [JsonProperty("longitude")]
+        public double Longitude {
+            get; set;
+        }
+
+        [JsonProperty("surveyors")]
+        public List<long> Surveyors {
+            get; set;
+        }
+
+
+    public partial class Surveyor {
+        [JsonProperty("id")]
+        public long Id {
+            get; set;
+        }
+
+        [JsonProperty("name")]
+        public string Name {
+            get; set;
+        }
+
+        [JsonProperty("contacts")]
+        public List<string> Contacts {
+            get; set;
+        }
+
+        [JsonProperty("prices")]
+        public List<string> Prices {
+            get; set;
+        }
+    }
+
+    public partial class PortsAndASurveyorsData {
+        public static PortsAndASurveyorsData FromJson(string json) => JsonConvert.DeserializeObject<PortsAndASurveyorsData>(json, Converter.Settings);
+    }
+
+    public static class Serialize {
+        public static string ToJson(this PortsAndASurveyorsData self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
