25b155a2c9ca99d800bab29252bb80718a9058f3
Sweeper777 <sumulang@gmail.com>
2018-06-10 10:30:43 +0100

    configured portsListBox

--- PortsAndSurveyors/Form1.Designer.cs
+++ PortsAndSurveyors/Form1.Designer.cs
@@ -91,6 +91,17 @@
             this.statusLabel.TabIndex = 2;
             this.statusLabel.Text = "Loading ports and surveyors data...";
             this.statusLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // portsListBox
+            // 
+            this.portsListBox.Font = new System.Drawing.Font("SimSun", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.portsListBox.FormattingEnabled = true;
+            this.portsListBox.ItemHeight = 15;
+            this.portsListBox.Location = new System.Drawing.Point(6, 52);
+            this.portsListBox.Name = "portsListBox";
+            this.portsListBox.Size = new System.Drawing.Size(284, 274);
+            this.portsListBox.TabIndex = 3;
+            this.portsListBox.SelectedIndexChanged += new System.EventHandler(this.portsListBox_SelectedIndexChanged);
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
