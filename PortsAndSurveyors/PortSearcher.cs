bdc9eb7af581ec4f382ea4e1c64dff400b868805
Sweeper777 <sumulang@gmail.com>
2018-06-16 08:54:27 +0100

    added PortSearcher

---
+++ PortsAndSurveyors/PortSearcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GMap.NET;
+
+namespace PortsAndSurveyors {
+    class PortSearcher {
+        public List<Port> Ports {
+            get;
+        }
+
+        public PortSearcher(List<Port> ports) {
+            Ports = ports;
+        }
+
+    }
+}
