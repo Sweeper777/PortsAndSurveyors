ef28e02849b539f1a3d89148967b6b00dadf62be
Sweeper777 <sumulang@gmail.com>
2018-06-16 08:55:09 +0100

    added DIstanceFrom extension method

--- PortsAndSurveyors/PortSearcher.cs
+++ PortsAndSurveyors/PortSearcher.cs
@@ -15,5 +15,32 @@ namespace PortsAndSurveyors {
             Ports = ports;
         }
 
+    static class PointLatLngHelper {
+        public static double DistanceFrom(this PointLatLng p1, PointLatLng p2) {
+            /*
+            Credits: https://stackoverflow.com/a/27943/5133585
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+            */
+            var R = 6371.0;
+            var dLat = (p2.Lat - p1.Lat).ToRadians();
+            var dLon = (p2.Lng - p1.Lng).ToRadians();
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Cos(p1.Lat.ToRadians()) * Math.Cos(p2.Lat.ToRadians()) *
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c;
+            return d;
+        }
+
     }
 }
